9e5b539550a80dca22f152cf1634c45597e66875
DeanGriggs <117603385+DeanGriggs@users.noreply.github.com>
2023-03-05 19:41:54 -0700

    Add files via upload

---
+++ Activity 3/Activity 3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a positive integer: ");
+            int number = int.Parse(Console.ReadLine());
+            int userInput = number;
+            while (number != 1)
+            {
+                Console.WriteLine(number + " ");
+                if (number % 2 == 0)
+                {
+                    number = number / 2;
+                }
+                else
+                {
+                    number = number * 3 + 1;
+                }
+            }
+
+            Console.WriteLine(number);
+            Console.ReadLine();
+
+
+
+
+            Console.Write("and now as a Factorial: ");
+            int number1 = userInput;
+
+            int factorial = 1;
+
+            for (int i = number1; i >= 1; i--)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+            Console.ReadLine();
+
+
+
+                int numberA = number1;
+
+            Console.WriteLine("Please enter a second positive integer: ");
+                int number2 = int.Parse(Console.ReadLine());
+
+
+            int answer = gcd(numberA, number2);
+                Console.Out.WriteLine("The gcd of {0} and {1} is {2}", numberA, number2, answer); Console.ReadLine();
+
+
+            int gcd(int n1, int n2)
+            {
+                
+                if (n2 == 0)
+                {
+                    return n1;
+                }
+                else
+                {
+                    Console.Out.WriteLine("Not yet. Remainder is {0}", n1 % n2);
+                    return gcd(n2, n1 % n2);
+                }
+                
+            }
+
+
+            
+        }
+    }
+}
---
+++ Activity 3/KnightsTour/KnightTour.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightsTour
+{
+    class KnightTour
+    {
+        private static readonly int[,] moves = new int[,] {
+        { 2, 1 }, { 1, 2 }, { -1, 2 }, { -2, 1 },
+        { -2, -1 }, { -1, -2 }, { 1, -2 }, { 2, -1 }
+    };
+
+        private int[,] board;
+        private int boardSize;
+        private bool solutionFound;
+
+        public KnightTour(int boardSize)
+        {
+            this.boardSize = boardSize;
+            board = new int[boardSize, boardSize];
+        }
+
+        public void Solve()
+        {
+            VisitSquare(0, 0);
+            TryNextMove(0, 0, 1);
+        }
+
+        private void VisitSquare(int row, int col)
+        {
+            board[row, col] = 1; // mark as visited
+        }
+
+        private bool IsValidMove(int row, int col)
+        {
+            return row >= 0 && row < boardSize &&
+                   col >= 0 && col < boardSize &&
+                   board[row, col] == 0;
+        }
+
+        private void TryNextMove(int row, int col, int moveNumber)
+        {
+            if (moveNumber == boardSize * boardSize)
+            {
+                solutionFound = true;
+                PrintSolution();
+                return;
+            }
+
+            for (int i = 0; i < moves.GetLength(0); i++)
+            {
+                int nextRow = row + moves[i, 0];
+                int nextCol = col + moves[i, 1];
+
+                if (IsValidMove(nextRow, nextCol))
+                {
+                    VisitSquare(nextRow, nextCol);
+                    TryNextMove(nextRow, nextCol, moveNumber + 1);
+                    if (solutionFound) return; // stop searching if a solution is found
+                    board[nextRow, nextCol] = 0; // unmark as visited (backtracking)
+                }
+            }
+        }
+
+        private void PrintSolution()
+        {
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    Console.Write("{0,3}", board[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
